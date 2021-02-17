f077517c2f71820fef4b450c8754a227d57eca5b
mahzza <mahsa.79mirmiran@gmail.com>
2021-02-17 12:44:33 +0330

    iphone and ipad ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,11 @@
 module.exports = {
   theme: {
     screens: {
-        'sm': '640px',
-        // => @media (min-width: 640px) { ... } 
-    
-        'md': '768px',
-        // => @media (min-width: 768px) { ... }
-    
-        'lg': '1024px',
-        // => @media (min-width: 1024px) { ... }
-    
-        'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
+      'xs': { 'max': '414px' },
+      // 'sm':{'max' : '768px'},
+      'md':{'min' : '768px'}
+      // 'laptop':{ 'min':'1024px'},
+      // 'desktop':{ 'min':'1280px'},
     },
     extend: {
       colors: {
